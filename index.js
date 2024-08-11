170b43b8f620575b48fc6db07c5ae42288b0d565
Kartik <kartik18badmera@gmail.com>
2024-08-11 14:35:09 +0530

    added multiple bricks

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const startButton = document.getElementById('start');
-    startButton.onclick = function(){
-        document.getElementById('heading').innerHTML="Game started";
-    }
+// oject for bricklayout freezed so can't change on live size change
+const brickLayout = Object.freeze({
+    height:15,
+    width:50
 });
+// spacing between bricks
+const spacing = Object.freeze({
+    horizontal: 10,
+    vertical: 15
+});
+// brick count will change it later according to the level
+const brickCount = Object({
+    row : 7,
+    column : 9
+});
+
+const initialBottom = 570;  // hardcoded for now
+var containerSelector = '#container';
+// brick class that is individual brick
+class Brick {
+    constructor(left, bottom) {
+        this.bottomLeft = { x: left, y: bottom };
+        this.bottomRight = { x: left + brickLayout.width, y: bottom };
+        this.topLeft = { x: left, y: bottom + brickLayout.height };
+        this.topRight = { x: left + brickLayout.width, y: bottom + brickLayout.height };
+    }
+    createBrick() {
+        const brickElement = document.createElement('div');
+        brickElement.classList.add('brick');
+        brickElement.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+        brickElement.style.left = `${this.bottomLeft.x}px`;
+        brickElement.style.bottom = `${this.bottomLeft.y}px`;
+        return brickElement;
+    }
+}
+
+// list of bricks
+class BrickWall {
+    constructor(containerSelector) {
+        this.container = document.querySelector(containerSelector);
+        this.bricks = [];
+    }
+    createBricks() {
+        for (let row = 1; row <= brickCount.row; row++) {
+            for (let col = 0; col < brickCount.column; col++) {
+                const left = spacing.horizontal + col * (brickLayout.width + spacing.horizontal);
+                const bottom = initialBottom - row * (brickLayout.height + spacing.vertical);
+                const brick = new Brick(left, bottom);
+                this.bricks.push(brick);
+            }
+        }
+    }
+    displayBricks() {
+        this.bricks.forEach(brick => {
+            this.container.appendChild(brick.createBrick());
+        });
+    }
+    buildWall() {
+        this.createBricks();
+        this.displayBricks();
+    }
+}
+const brickWall = new BrickWall(containerSelector);
+brickWall.buildWall();
